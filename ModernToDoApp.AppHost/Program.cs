c540b201748e9c46099494bf9bc5d12efdcb856a
Ramakrishna Gummadi <klick.ram@hotmail.com>
2024-08-06 13:04:21 +0530

    microservice - user service

--- ModernToDoApp.AppHost/Program.cs
+++ ModernToDoApp.AppHost/Program.cs
@@ -1,4 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 builder.AddProject<Projects.ModernToDoApp>(name: "webapp");
 builder.AddProject<Projects.TaskService>(name: "taskservice");
+builder.AddProject<Projects.UserService>(name: "userservice");
 builder.Build().Run();

04567ba02597d5fe86b28427feb9778a08cc452d
Ramakrishna Gummadi <klick.ram@hotmail.com>
2024-08-06 13:02:03 +0530

    Added microservice - task service

--- ModernToDoApp.AppHost/Program.cs
+++ ModernToDoApp.AppHost/Program.cs
@@ -1,3 +1,4 @@
 var builder = DistributedApplication.CreateBuilder(args);
 builder.AddProject<Projects.ModernToDoApp>(name: "webapp");
+builder.AddProject<Projects.TaskService>(name: "taskservice");
 builder.Build().Run();
